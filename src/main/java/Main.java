70fa26f97185a6ca82bb67e8493c7784b2e6a36e
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-04-17 14:45:47 +0000

    Gotowy kod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("wprowadz liczbe wierszy ");
+        int x = scanner.nextInt();
+
+        System.out.print("wprowadz liczbe kolumn");
+        int y = scanner.nextInt();
+
         char znak = '*';
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
                 System.out.print(znak + " ");
             }
             System.out.println();
