53585d89bfef43cc0c642a27803b156e7170de45
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-04-17 14:32:41 +0000

    Petla

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,10 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   char znak='*';
+    for (int i=0; i<5; i++) {
+      
+    }
   }
 
   // @Test
